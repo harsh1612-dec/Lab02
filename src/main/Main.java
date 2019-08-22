21f722900ef19b4bae9c03f35adcc1de6fb48ed6
Harsh1612-dec <harsh.agarwal_cs18@gla.ac.in>
2019-08-22 15:13:27 +0530

    create object of scanner class

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,15 @@
 package main;
 
+import java.util.Scanner;
+
 /**
  * @author Harsh Agarwal
  * @version 1.0
  */
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String userName = "";
+        scanner.close();
     }
 }
